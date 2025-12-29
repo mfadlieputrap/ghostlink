4b9b2c5f82d290d7b1161794efebd56790142481
mfadlieputrap <mfadlieputrap@gmail.com>
2025-12-29 08:07:38 +0700

    chore: change url backend

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 
     try {
       // Kirim ke backend
-      const res = await fetch('http://localhost:3000/messages', {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/messages`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ content: message }),
